a1c6ca7662e2bc7d9e0841afc43fb56c45b8b44d
Bryan Leung <blkclkc@gmail.com>
2020-10-27 10:26:19 -0400

    clean up

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,35 +1,7 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
 import moment from 'moment'
 
-import { fetchOrderSuccess } from '../actions'
-
 export default function Table({ orders }) {
-  /* const original_orders = useSelector(state => (Object.values(state.orders) || []))
-  const [orders, setOrders] = useState(original_orders)
-
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    fetchOrders()
-  }, [])
-
-  useEffect(() => {
-    let updated_orders = [...original_orders]
-    if(selectedSupplier !== 'all') {
-      updated_orders = original_orders.filter(o => o.vendorName === selectedSupplier)
-    }
-    setOrders(updated_orders)
-  }, [selectedSupplier])
-
-  async function fetchOrders() {
-    await fetch('https://chefhero.free.beeceptor.com/')
-      .then(res => res.json())
-      .then(json => {
-        dispatch(fetchOrderSuccess(json.data))
-      })
-  } */
-
   function renderRow(order, i) {
     const tagStyle = order.orderBuyerStatus === 'Paid' ? { backgroundColor: '#b0ddcb' } : { backgroundColor: '#c4c4ff' }
 
